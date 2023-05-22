7dc5b18794456ffe2f1843c1ba6f6b17d4eb9660
markogarza <markogarza@gmail.com>
2023-05-22 12:30:58 -0400

    Event-loop file done

--- eventloop.js
+++ eventloop.js
@@ -1,19 +1,33 @@
 const fs = require("fs");
+const crypto = require("crypto");
 
-setTimeout(() => console.log("Timer 1 finished!"), 0);
-setImmediate(() => console.log("Immidiate 1 finished"));
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 4;
 
-const testFile = fs.readFile("test-file.txt", (err, data) => {
-  console.log("I/O finished!");
+setTimeout(() => console.log("Timer 1 finished"), 0);
+setImmediate(() => console.log("Immediate 1 finished"));
+
+fs.readFile("test-file.txt", () => {
+  console.log("I/O finished");
   console.log("----------------");
 
-  setTimeout(() => console.log("Timer 2 finished!"), 0);
-  setTimeout(() => console.log("Timer 3 finished!"), 3000);
-  setImmediate(() => console.log("Immidiate 2 finished"));
+  setTimeout(() => console.log("Timer 2 finished"), 0);
+  setTimeout(() => console.log("Timer 3 finished"), 3000);
+  setImmediate(() => console.log("Immediate 2 finished"));
 
-  process.nextTick(() => {
-    console.log("Process nextTick done");
-  });
+  process.nextTick(() => console.log("Process.nextTick"));
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password encrypted");
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password encrypted");
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password encrypted");
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password encrypted");
 });
 
-console.log("Top-level code executed!");
+console.log("Hello from the top-level code");
